0358633bc2e8cd3f84e1d0c0b72423509ec880da
WadoodAlli <160502372+WadoodAlli@users.noreply.github.com>
2024-08-12 14:52:20 +0100

    Update Product.java

--- catalogue/Product.java
+++ catalogue/Product.java
@@ -13,7 +13,8 @@ import java.io.Serializable;
 public class Product implements Serializable
 {
   private static final long serialVersionUID = 20092506;
-  private String theProductNum;       // Product number
+  private String theCatNum;       // Product number
+  private String theCatBreed;     // Breed of the cat
   private String theDescription;      // Description of product
   private double thePrice;            // Price of product
   private int    theQuantity;         // Quantity involved
@@ -25,16 +26,18 @@ public class Product implements Serializable
    * @param aPrice The price of the product
    * @param aQuantity The Quantity of the product involved
    */
-  public Product( String aProductNum, String aDescription,
+  public Product( String aCatNum, String aDescription,
                   double aPrice, int aQuantity )
   {
-    theProductNum  = aProductNum;     // Product number
+    theCatNum  = aCatNum;     // Product number
+    theCatBreed = aCatBreed;      // Breed of the cat
     theDescription = aDescription;    // Description of product
     thePrice       = aPrice;          // Price of product
     theQuantity    = aQuantity;       // Quantity involved
   }
   
-  public String getProductNum()  { return theProductNum; }
+  public String getCatNum()  { return theCatNum; }
+  public String getCatBreed() { return theCatBreed; }
   public String getDescription() { return theDescription; }
   public double getPrice()       { return thePrice; }
   public int    getQuantity()    { return theQuantity; }
@@ -45,6 +48,10 @@ public class Product implements Serializable
   }
   
   public void setDescription( String aDescription )
+  { 
+    theDescription = aDescription;
+  }
+    public void setCatBreed( String aCatBreed )
   { 
     theDescription = aDescription;
   }
